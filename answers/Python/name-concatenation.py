63316e2efb050acaa259322655d28f99a29b27c0
Flipponachi <iamflipponachi@gmail.com>
2019-06-08 21:02:39 +0100

    Completed first question

---
+++ answers/Python/name-concatenation.py
@@ -0,0 +1,36 @@
+#Solution to First task about Name concatenation
+
+#import external methods for exercise
+import utils
+
+first_Name = input("Hello, What's your first name? ")
+
+while(not utils.checkEmptyString(first_Name)):
+    first_Name = input("We didn't get that before. Input your name please we beg ")
+
+
+last_Name = input("Okay. What's your last name? ")
+while(not utils.checkEmptyString(last_Name)):
+    last_Name = input("Just like before we didn't get your last name. Input your last name please we beg ")
+
+age = input(f"Almost done {first_Name}, how old are you? ")
+while(not utils.checkEmptyString(age) or not utils.isValidNumber(age)):
+    age = input("We didn't get that before. Input your age please we beg ")
+
+
+user_gender = input(f"Last question, we promise. What's your gender? ")
+while(not utils.checkEmptyString(user_gender)):
+    user_gender = input("We didn't get that before. Tell us your gender ")
+
+yearOfBirth = 0
+try:
+    yearOfBirth = utils.yearOfBirthCalculator(int(age))
+except Exception as error:
+    print(error)
+
+
+print(f"Welcome, {first_Name} {last_Name} {yearOfBirth}")
+
+
+
+
---
+++ answers/Python/utils.py
@@ -0,0 +1,25 @@
+import datetime
+#Calculate year of birth
+def yearOfBirthCalculator(age):
+
+    if age < 1:
+        raise ArithmeticError(f"Age cannot be zero, {age}")
+        
+    currentYear = datetime.datetime.now()
+    year = currentYear.year - age
+    return year
+
+#Check for empty strings from user input
+def checkEmptyString(stringVariable):
+    if stringVariable == "":
+        return False
+    
+    return True
+
+#Check if string input is a valid number
+def isValidNumber(stringInt):
+    try:
+        int(stringInt)
+        return True
+    except ValueError:
+        return False
