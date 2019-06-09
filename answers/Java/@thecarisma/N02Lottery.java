a316bb53d3529403e3c8732446724c19b494322c
Azeez Adewale <azeezadewale98@gmail.com>
2019-06-09 09:17:15 +0100

    resolve issue with N02Lottery double response

--- answers/Java/@thecarisma/N02Lottery.java
+++ answers/Java/@thecarisma/N02Lottery.java
@@ -1,6 +1,7 @@
 
 import java.io.IOException;
 import java.util.Random;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -18,24 +19,20 @@ import java.util.logging.Logger;
 public class N02Lottery {
     
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int a, b, c ;
         while (true) {
-            try {
-                Random random = new Random();
-                int a, b, c ;
-
-                System.out.print("Press Enter key to conitue...");
-                System.in.read();
-                a = random.nextInt(10);
-                b = random.nextInt(10);
-                c = random.nextInt(10);
-                System.out.printf("%d %d %d : ", a, b, c);
-                if ( a == 7 || b == 7 || c == 7)
-                    System.out.println("Congratulations!");
-                else
-                    System.out.println("Try again! Better luck next time.");
-            } catch (IOException ex) {
-                Logger.getLogger(N02Lottery.class.getName()).log(Level.SEVERE, null, ex);
-            }
+            System.out.print("Press Enter key to conitue...");
+            scanner.nextLine();
+            a = random.nextInt(10);
+            b = random.nextInt(10);
+            c = random.nextInt(10);
+            System.out.printf("%d %d %d : ", a, b, c);
+            if ( a == 7 || b == 7 || c == 7)
+                System.out.println("Congratulations!");
+            else
+                System.out.println("Try again! Better luck next time.");
         }
     }
     
