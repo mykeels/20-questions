52ccc3a3df80f594b35feaafb07cdecb787ee663
Oseme Odigie <osemeodigie@yahoo.com>
2019-06-17 16:16:43 +0100

    feat: added the solution to the third question

---
+++ Python/@oseme-techguy/03-word-in-reverse.py
@@ -0,0 +1,14 @@
+"""
+    Solution to Word in Reverse
+"""
+
+if __name__ == '__main__':
+    word = input('Enter a word: ')
+    word = str(word)
+
+    i = len(word)
+    reversed_word = ''
+    while i > 0:
+        reversed_word += word[i - 1]
+        i -= 1
+    print('{reversed_word}\n'.format(reversed_word=reversed_word))
